efcd36a14c98c37a22464e5f8b1287c36b1ccb18
Alexander Zinchenko <az67128@gmail.com>
2019-02-03 11:29:57 +0300

    version 2

---
+++ precache-manifest.c3530362d83d7b92434e01d548bcc708.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0f2ef3f6cb27f36e59a4",
+    "url": "/static/js/main.0f2ef3f6.chunk.js"
+  },
+  {
+    "revision": "db02576b01e290b90870",
+    "url": "/static/js/1.db02576b.chunk.js"
+  },
+  {
+    "revision": "0f2ef3f6cb27f36e59a4",
+    "url": "/static/css/main.8ed07bba.chunk.css"
+  },
+  {
+    "revision": "fd7ba18a80568c09df6a99a6be916688",
+    "url": "/index.html"
+  }
+];
